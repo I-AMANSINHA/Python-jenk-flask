1e797bc6f166fcfdc921a4c8b326fb908dff44ea
aman-sinha <amansinha4698@gmail.com>
2026-06-25 11:27:50 +0530

    added files

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from flask import Flask
+from db import get_connection
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Python Flask App Running"
+
+    conn = get_connection()
+
+    cur = conn.cursor()
+
+    cur.execute("SELECT version();")
+
+    version = cur.fetchone()
+
+    cur.close()
+    conn.close()
+
+    return f"Connected to PostgreSQL: {version[0]}"
+
 
 @app.route("/health")
 def health():
     return "UP"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
